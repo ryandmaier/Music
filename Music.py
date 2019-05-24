981a9de3ae6b007ccfe8713066be75348ed23794
Ryan Maier <ryandmaier@gmail.com>
2019-05-23 22:11:37 -0700

    output sound yo

--- Music.py
+++ Music.py
@@ -4,15 +4,55 @@ import numpy
 import struct
 import cmath
 import math
+from pyaudio import PyAudio
+
+try:
+    from itertools import izip
+except ImportError: # Python 3
+    izip = zip
+    xrange = range
 
 class Music:
 
   def main(self):
     print('hi')
-    all = self.getSound(128, 1) # 344, 256
-    print(self.getCosData(1,300,1))
+    self.outputSound(600)
+    #all = self.getSound(128, 1) # 344, 256
+    #print(self.getCosData(1,300,1))
     #all = getSound(128, 0.5) # 82 sublists, each 512 long
     #all = getSound(256, 0.5) # 43 sublists, each 1024 long
+
+  def outputSound(self, freq):
+    for i in range(0,10,):
+        self.sine_tone(freq+i*20, 1)
+
+  def sine_tone(self, frequency, duration, volume=1, sample_rate=22050):
+    n_samples = int(sample_rate * duration)
+    restframes = n_samples % sample_rate
+
+    p = PyAudio()
+    stream = p.open(format=p.get_format_from_width(1), # 8bit
+                    channels=1, # mono
+                    rate=sample_rate,
+                    output=True)
+    s = lambda t: volume * math.sin(2 * math.pi * frequency * t / sample_rate)
+    samples = (int(s(t) * 0x7f + 0x80) for t in xrange(n_samples))
+    for buf in izip(*[samples]*sample_rate): # write several samples at a time
+        stream.write(bytes(bytearray(buf)))
+
+    # fill remainder of frameset with silence
+    #stream.write(b'\x80' * restframes)
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+  def getFreqList(self, intensities):
+    freqs = [] # 1 value for each piece
+    for i in range(len(intensities)):
+        piece = intensities[i] # sublist
+        for i in range(0,len(piece)):
+            print("hold up")
 
   def getCosData(self, freq, numData, time):
     all = []
